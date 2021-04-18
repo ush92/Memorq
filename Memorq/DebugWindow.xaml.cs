de2060652bc5096bb6d627e415db91984f0083f7
ush <ush1992@gmail.com>
2021-04-19 00:26:27 +0200

    added status bar

---
+++ Memorq/DebugWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Memorq.Classes.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memorq
+{
+    /// <summary>
+    /// Interaction logic for DebugWindow.xaml
+    /// </summary>
+    public partial class DebugWindow : Window
+    {
+        List<Category> categories;
+        public DebugWindow()
+        {
+            InitializeComponent();
+
+            categories = new List<Category>();
+        }
+
+        private void insertDBtestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Category category = new Category()
+            {
+                Name = insertDBtxt.Text
+            };
+
+            if (category.Name.Equals(string.Empty)) category.Name = "default";
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Category>();
+                connection.Insert(category);
+            }
+        }
+
+        private void readDBbtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Category>();
+                categories = (connection.Table<Category>().ToList()).OrderBy(c => c.Name).ToList();
+            }
+
+            if (categories != null)
+            {
+                categoriesListView.ItemsSource = categories;
+            }
+        }
+    }
+}
+
+
+/*       
+       algorithm SM-2 is
+input:  user grade q
+       repetition number n
+       easiness factor EF
+       interval I
+output: updated values of n, EF, and I
+
+if q ≥ 3 (correct response) then
+   if n = 0 then
+       I ← 1
+   else if n = 1 then
+       I ← 6
+   else
+       I ← ⌈I × EF⌉
+   end if
+   EF ← EF + (0.1 − (5 − q) × (0.08 + (5 − q) × 0.02))
+   if EF < 1.3 then
+       EF ← 1.3
+   end if
+   increment n
+else (incorrect response)
+   n ← 0
+   I ← 1
+end if
+
+return (n, EF, I)
+
+*/
--- Memorq/MainWindow.xaml.cs
+++ Memorq/MainWindow.xaml.cs
@@ -22,73 +22,16 @@ namespace Memorq
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Category> categories;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            categories = new List<Category>();
         }
 
-        private void insertDBtestBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Category category = new Category()
-            {
-                Name = insertDBtxt.Text
-            };
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    DebugWindow debugWindow = new DebugWindow();
+        //    debugWindow.Show();
+        //}
 
-            if (category.Name.Equals(string.Empty)) category.Name = "default";
-
-            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
-            {
-                connection.CreateTable<Category>();
-                connection.Insert(category);
-            }
-        }
-
-        private void readDBbtn_Click(object sender, RoutedEventArgs e)
-        {
-            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
-            {
-                connection.CreateTable<Category>();
-                categories = (connection.Table<Category>().ToList()).OrderBy(c => c.Name).ToList();
-            }
-
-            if(categories != null)
-            {
-                categoriesListView.ItemsSource = categories;
-            }
-        }
     }
 }
-
-             /*       
-                    algorithm SM-2 is
-            input:  user grade q
-                    repetition number n
-                    easiness factor EF
-                    interval I
-            output: updated values of n, EF, and I
-
-            if q ≥ 3 (correct response) then
-                if n = 0 then
-                    I ← 1
-                else if n = 1 then
-                    I ← 6
-                else
-                    I ← ⌈I × EF⌉
-                end if
-                EF ← EF + (0.1 − (5 − q) × (0.08 + (5 − q) × 0.02))
-                if EF < 1.3 then
-                    EF ← 1.3
-                end if
-                increment n
-            else (incorrect response)
-                n ← 0
-                I ← 1
-            end if
-
-            return (n, EF, I)
-
-         */
