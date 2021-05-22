1e19c17fe22605f17390058174c96a39730f12bb
ush <ush1992@gmail.com>
2021-05-22 22:43:30 +0200

    added new icons and mainWindow tooltips

--- Memorq/ViewModels/MainWindowViewModel.cs
+++ Memorq/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,64 @@
 ﻿using Memorq.Infrastructure;
+using Memorq.Models;
+using Memorq.Services;
 using Memorq.Views;
 using Memorq.Views.Dialogs;
-using System.Windows;
 using System.Windows.Input;
 
 namespace Memorq.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        private readonly ICategoryService _categoryProvider;
+        private readonly IItemService _itemProvider;
         private readonly IWindowFactory _windowFactory;
 
-        public MainWindowViewModel(IWindowFactory windowFactory)
+        private Category _defaultCategory;
+        private string _defaultCategoryName;
+
+        public Category DefaultCategory
         {
+            get => _defaultCategory;
+            set
+            {
+                _defaultCategory = value;
+                OnPropertyChanged(nameof(DefaultCategory));
+            }
+        }
+
+        public string DefaultCategoryName
+        {
+            get => _defaultCategoryName;
+            set
+            {
+                _defaultCategoryName = value;
+                OnPropertyChanged(nameof(DefaultCategoryName));
+            }
+        }
+
+        private void InitDefaultCategory()
+        {
+            DefaultCategory = _categoryProvider.GetCategory(UserSettings.Default.DefaultCategory);
+            if (DefaultCategory == null)
+            {
+                DefaultCategoryName = GetDictResource("DefaultCategoryNotChosen");
+
+                UserSettings.Default.DefaultCategory = -1;
+                UserSettings.Default.Save();
+            }
+            else
+            {
+                DefaultCategoryName = string.Format("{0}: {1}", GetDictResource("Category"), DefaultCategory.Name);
+            }
+        }
+
+        public MainWindowViewModel(ICategoryService categoryProvider, IItemService itemProvider, IWindowFactory windowFactory)
+        {
+            _categoryProvider = categoryProvider;
+            _itemProvider = itemProvider;
             _windowFactory = windowFactory;
+
+            InitDefaultCategory();
         }
 
         public ICommand ShowCategoryManagerCommand => new RelayCommand(_ =>
@@ -22,6 +68,9 @@ namespace Memorq.ViewModels
             if (categoryManager.ShowDialog() == true)
             {
                 var categoryManagerViewModel = (CategoryManagerViewModel)categoryManager.DataContext;
+
+                DefaultCategory = categoryManagerViewModel.SelectedCategory;
+                DefaultCategoryName = string.Format("{0}: {1}", GetDictResource("Category"), DefaultCategory.Name);
 
                 UserSettings.Default.DefaultCategory = categoryManagerViewModel.SelectedCategory.Id;
                 UserSettings.Default.Save();
--- Memorq/Views/MainWindow.xaml.cs
+++ Memorq/Views/MainWindow.xaml.cs
@@ -10,6 +10,49 @@ namespace Memorq.Views
             InitializeComponent();
             DataContext = mainWindowViewModel;
             mainWindowViewModel.OwnerWindow = this;
+
+            TooltipLabel.Content = (string)Application.Current.FindResource("DefaultTooltip");
         }
+
+
+        #region Tooltips
+
+        private void LearnBtn_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TooltipLabel.Content = (string)Application.Current.FindResource("BtnLearnTooltip");
+        }
+        private void LearnBtn_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TooltipLabel.Content = (string)Application.Current.FindResource("DefaultTooltip");
+        }
+
+        private void AddBtn_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TooltipLabel.Content = (string)Application.Current.FindResource("BtnAddTooltip");
+        }
+        private void AddBtn_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TooltipLabel.Content = (string)Application.Current.FindResource("DefaultTooltip");
+        }
+
+        private void NewBtn_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TooltipLabel.Content = (string)Application.Current.FindResource("BtnNewTooltip");
+        }
+        private void NewBtn_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TooltipLabel.Content = (string)Application.Current.FindResource("DefaultTooltip");
+        }
+
+        private void ForceBtn_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TooltipLabel.Content = (string)Application.Current.FindResource("BtnForceTooltip");
+        }
+        private void ForceBtn_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TooltipLabel.Content = (string)Application.Current.FindResource("DefaultTooltip");
+        }
+
+        #endregion
     }
 }
