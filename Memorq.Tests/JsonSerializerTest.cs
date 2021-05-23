4bbb875b5304c355d95dbe8bb80d577b58d148d5
ush <ush1992@gmail.com>
2021-05-24 01:40:00 +0200

    changed JsonSerializerTest to verify JsonIgnore fields in ItemModel

--- Memorq.Tests/JsonSerializerTest.cs
+++ Memorq.Tests/JsonSerializerTest.cs
@@ -10,12 +10,27 @@ namespace Memorq.Tests
 {
     public class JsonSerializerTest
     {
-        private readonly Fixture fixture = new Fixture();
+        private readonly Fixture fixture = new();
 
         [Fact]
-        public void JsonSerializerWorksProperly()
+        public void JsonSerializerWorksProperlyWithIgnoreJsonAtributes()
         {
+            fixture.Customize<Item>(o => o
+               .With(i => i.Question)
+               .With(i => i.Answer)
+
+               .Without(i => i.CategoryId)
+               .Without(i => i.EFactor)
+               .Without(i => i.Id)
+               .Without(i => i.InsertDate)
+               .Without(i => i.Interval)
+               .Without(i => i.LastGrade)
+               .Without(i => i.LastRepetitionDate)
+               .Without(i => i.Repetition)
+               );
+
             var listToSerialize = fixture.CreateMany<Item>().ToList();
+
             var jsonString = JsonSerializer.Serialize(listToSerialize);
             var deserializedList = JsonSerializer.Deserialize<List<Item>>(jsonString);
 
