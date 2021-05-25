968045f3cd60d5dd0e5e0610c27fd570ca35aee5
ush <ush1992@gmail.com>
2021-05-25 17:11:32 +0200

    fix MemorqCoreTest

--- Memorq.Tests/MemorqCoreTest.cs
+++ Memorq.Tests/MemorqCoreTest.cs
@@ -20,7 +20,19 @@ namespace Memorq.Tests
         }
 
         [Fact]
-        public void IfNewItemGradeIs5EFactorEquals2_6()
+        public void IfNewItemGradeIsZeroThenEFactorEquals1_7000000000000002()
+        {
+            var newItem = CreateDefaultItem();
+            int grade = 0;
+
+            MemorqCore memorqCore = new();
+            memorqCore.UpdateItemStats(newItem, grade);
+
+            newItem.EFactor.Should().Be(1.7000000000000002);
+        }
+
+        [Fact]
+        public void IfNewItemGradeIsFiveThenEFactorEquals2_6()
         {
             var newItem = CreateDefaultItem();
             int grade = 5;
@@ -47,6 +59,38 @@ namespace Memorq.Tests
         }
 
         [Fact]
+        public void FirstIntervalsAreAlwaysTheSame()
+        {
+            var newItem1 = CreateDefaultItem();
+            int grade1 = 5;
+
+            var newItem2 = CreateDefaultItem();
+            int grade2 = 0;
+
+            MemorqCore memorqCore = new();
+            memorqCore.UpdateItemStats(newItem1, grade1);
+            memorqCore.UpdateItemStats(newItem2, grade2);
+
+            newItem1.Interval.Should().Be(newItem2.Interval);
+        }
+
+        [Fact]
+        public void IfItemGetsThreePositiveGradesInRowThenRepetitionEqualsThree()
+        {
+            var newItem = CreateDefaultItem();
+            int grade0 = 3;
+            int grade1 = 4;
+            int grade2 = 5;
+
+            MemorqCore memorqCore = new();
+            memorqCore.UpdateItemStats(newItem, grade0);
+            memorqCore.UpdateItemStats(newItem, grade1);
+            memorqCore.UpdateItemStats(newItem, grade2);
+
+            newItem.Repetition.Should().Be(3);
+        }
+
+        [Fact]
         public void IfTwoItemsFirstGradesAreDifferentAndNextGradesAreTheSameThenTheirIntervalsAreDifferent()
         {
             var newItem1 = CreateDefaultItem();
@@ -68,7 +112,22 @@ namespace Memorq.Tests
         }
 
         [Fact]
-        public void IfItemGetsOnlyNegativeGradesInRowThenRepetitionEqualsZero()
+        public void IntervalCannotBeGreaterThan712()
+        {
+            var newItem = CreateDefaultItem();
+            int grade = 5;
+
+            MemorqCore memorqCore = new();
+            for (int i = 0; i < 10; i++)
+            {
+                memorqCore.UpdateItemStats(newItem, grade);
+            }
+
+            newItem.Interval.Should().Be(712);
+        }
+
+        [Fact]
+        public void IfItemGetsThreeNegativeGradesInRowThenRepetitionEqualsZero()
         {
             var newItem = CreateDefaultItem();
             int grade0 = 0;
@@ -84,28 +143,27 @@ namespace Memorq.Tests
         }
 
         [Fact]
-        public void IfItemGetsThreePositiveGradesInRowThenRepetitionEqualsThree()
+        public void IfItemGetsThreeGrades3InRowThenIntervalEqualsThirteen()
         {
             var newItem = CreateDefaultItem();
-            int grade0 = 3;
-            int grade1 = 4;
-            int grade2 = 5;
+            int grade = 3;
 
             MemorqCore memorqCore = new();
-            memorqCore.UpdateItemStats(newItem, grade0);
-            memorqCore.UpdateItemStats(newItem, grade1);
-            memorqCore.UpdateItemStats(newItem, grade2);
+            memorqCore.UpdateItemStats(newItem, grade);
+            memorqCore.UpdateItemStats(newItem, grade);
+            memorqCore.UpdateItemStats(newItem, grade);
 
-            newItem.Repetition.Should().Be(3);
+            newItem.Interval.Should().Be(13);
         }
 
         [Fact]
-        public void IfItemGetsThreePositiveGradesInRowAndNextNegativeGradeThenRepetitionEqualsZero()
+        public void IfItemGetsThreePositiveGradesInRowAndNextGradeIsNegativeThenRepetitionEqualsZero()
         {
             var newItem = CreateDefaultItem();
             int grade0 = 3;
             int grade1 = 4;
             int grade2 = 5;
+
             int grade3 = 0;
 
             MemorqCore memorqCore = new();
@@ -115,21 +173,6 @@ namespace Memorq.Tests
             memorqCore.UpdateItemStats(newItem, grade3);
 
             newItem.Repetition.Should().Be(0);
-        }
-
-        [Fact]
-        public void IntervalCannotBeGreaterThan712()
-        {
-            var newItem = CreateDefaultItem();
-            int grade = 5;
-
-            MemorqCore memorqCore = new();
-            for (int i = 0; i < 10; i++)
-            {
-                memorqCore.UpdateItemStats(newItem, grade);
-            }
-
-            newItem.Interval.Should().Be(712);
         }
     }
 }
